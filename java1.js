94a0ca3e5d5f5310b086ace230b73de4404a4a33
Martina <martinamelillo97@gmail.com>
2018-06-15 22:03:25 +0200

    web

---
+++ java1.js
@@ -0,0 +1,13 @@
+$("title").ready(function() {
+  $('body').css('display', 'none');
+  $('body').fadeIn(1500);
+  $('.link').click(function(event) {
+    event.preventDefault();
+    newLocation = this.href;
+    $('body').fadeOut(1500, newpage);
+});
+
+function newpage() {
+  window.location = newLocation;
+}
+});
---
+++ pagine/java.js
@@ -0,0 +1,14 @@
+$("nav a").ready(function() {
+  $('body').css('display', 'none');
+  $('body').fadeIn(1500);
+  $('.link').click(function(event) {
+    event.preventDefault();
+    newLocation = this.href;
+    $('body').fadeOut(1500, newpage);
+});
+
+function newpage() {
+  window.location = newLocation;
+}
+});
+/*-------------------------------------------------*/
